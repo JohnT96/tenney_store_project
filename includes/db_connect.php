760755d540eb84bb222bd27a79b0c94cba3e59b0
JohnT96 <JOHTEN7331@students.ecpi.edu>
2026-02-15 21:52:12 -0500

    Add charset setting for database connection

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,4 +1,5 @@
 <?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $servername = "localhost";
 $username = "root";
@@ -6,6 +7,7 @@ $password = "";
 $database = "tenney_store_project";
 
 $conn = new mysqli($servername, $username, $password, $database);
+$conn->set_charset("utf8mb4");
 
 if ($conn->connect_error) {
     die("connection failed: " . $conn->connect_error);
