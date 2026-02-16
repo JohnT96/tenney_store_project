0aeeecd2ad5d52b10c2a863a152b2f4bed4f7547
JohnT96 <JOHTEN7331@students.ecpi.edu>
2026-02-15 21:55:04 -0500

    Implement product listing in catalog page
    Added database connection and product listing functionality to the catalog page.

--- pages/catalog.php
+++ pages/catalog.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+require_once "../includes/db_connect.php";
+
+$sql = "SELECT product_id, product_name, product_description, product_price FROM products";
+$result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +14,33 @@ session_start();
 <body>
 
 <h1>Catalog Page</h1>
-<p>If you can see this, the file path works!</p>
+<p><a href="add_product.php">Add a Product</a></p>
+<hr>
+<?php if ($result && $result->num_rows > 0): ?>
+    <ul>
+        <?php while ($row = $result->fetch_assoc()): ?>
+            <li>
+                <strong><?php echo htmlspecialchars($row['product_name']); ?></strong><br>
+                <?php echo htmlspecialchars($row['product_description']); ?><br>
+                $<?php echo number_format((float)$row['product_price'], 2); ?><br>
+
+                <a href="delete_product.php?id=<?php echo $row['product_id']; ?>"
+                    onclick="return confirm('Are you sure you want to delete this product?');">
+                    Delete
+                </a>
+
+                <br>
+
+                <a href="edit_product.php?id=<?php echo$row['product_id']; ?>">
+                Edit
+            </a>
+            </li>
+            <hr>
+        <?php endwhile; ?>
+    </ul>
+<?php else: ?>
+    <p>No products found.</p>
+<?php endif; ?>
 
 <a href="../index.php">Back to home</a>
 
